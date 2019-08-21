1c18997a43b53bf8ed9d35215236f3eb526398bc
shuaydali <SAli5@GBRDCLFMD014>
2019-08-21 10:55:13 +0100

    6122 - deletion of files no longer in use.

--- web/src/session/root-thunk.tsx
+++
@@ -1,7 +0,0 @@
-import { ThunkAction, ThunkDispatch } from 'redux-thunk';
-import { Action } from 'redux';
-
-export type RootState = {};
-export type ExtraArg = undefined;
-export type ThunkResult<R> = ThunkAction<R, RootState, ExtraArg, Action>;
-export type ThunkDispatch = ThunkDispatch<RootState, ExtraArg, Action>;
