6863d600ac3c9e319e354f69204678da233f036c
shuaydali <SAli5@GBRDCLFMD014>
2019-08-22 13:26:45 +0100

    6122-addnew-project merge conflicts

---
+++ web/src/components/Table.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Table from '../components/Table';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table IsSuperManager={ true } />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
