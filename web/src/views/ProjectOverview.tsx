9dedf4ced95e65cc9887618673c3399c5e28dd38
shuaydali <SAli5@GBRDCLFMD014>
2019-09-24 14:31:43 +0100

    6129 - update to general table to include code to handle header name and data being displayed in the table

--- web/src/views/ProjectOverview.tsx
+++ web/src/views/ProjectOverview.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import HeaderPage from '../components/HeaderPage/HeaderPage';
 import GeneralTable from '../components/Table/General';
-import { Grid, FormControl, InputLabel, TextField, Container } from '@material-ui/core';
+import { Grid, Container } from '@material-ui/core';
 import ProjectOverviewForm from '../components/Forms/ProjectOverviewForm';
+import { IGeneralTableHeaderProps, IGeneralTableProps } from '../components/Table/General/props';
+
+const tableHeaders: IGeneralTableHeaderProps[] = [
+    { heading: 'End Client Name', subHeading: 'ING' },
+    { heading: 'Project Name', subHeading: 'Building Maintainance' },
+    { heading: 'Project ID', subHeading: 'ING65956' },
+    { heading: 'CN Number', subHeading: 'ING89855' }
+];
+
+const table: IGeneralTableProps = {
+    headers: tableHeaders,
+    content: 'Working on new structure of a building with lighting',
+    editActionClick: () => { alert("You clicked me"); }
+}
 
 class ProjectOverview extends React.Component {
-
     render() {
         return <React.Fragment>
             <Container component="main" >
                 <HeaderPage Title={'Project Overview'} ActionList={[]} />
                 <Grid spacing={3} container>
                     <Grid item xs={12} sm={12}>
-                        <GeneralTable />
+                        <GeneralTable {...table} />
                     </Grid>
                     <Grid item xs={12} sm={12}>
                         <ProjectOverviewForm />
