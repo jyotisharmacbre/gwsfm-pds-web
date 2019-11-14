088419b291a88e3c9658b6a32f119dbfb7cc7eaa
himanshi-gulati <56675259+himanshi-gulati@users.noreply.github.com>
2019-11-14 21:43:12 +0530

    Update validate.test.tsx

--- web/src/components/Forms/ProjectForm/validate.test.tsx
+++ web/src/components/Forms/ProjectForm/validate.test.tsx
@@ -18,56 +18,5 @@ describe('Project Form Validation', () => {
       error = validate(expectedProps);
       expect(error.projectName).toBeDefined();
     });
-    /*
-        it('Company name is required',()=>{
-            expect(error.companyName).toBeDefined(); 
-        });
-        it('Contract name is required',()=>{
-            expect(error.contractName).toBeDefined(); 
-        });
-        it('Head of project name is required',()=>{
-            expect(error.projectHead).toBeDefined(); 
-        });
-        it('Project owner is required',()=>{
-            expect(error.projectOwner).toBeDefined(); 
-        });
-        it('Project manager name is required',()=>{
-            expect(error.projectManager).toBeDefined(); 
-        });
-
-        it('Project scope is required',()=>{
-            expect(error.projectScope).toBeDefined(); 
-        });
-        it('Project status is required',()=>{
-            expect(error.projectStatus).toBeDefined(); 
-        });
-        it('Country name is required',()=>{
-            expect(error.country).toBeDefined(); 
-        });
-        it('Probability of winning is required',()=>{
-            expect(error.winProbabilty).toBeDefined(); 
-        });
-        it('Currency is required',()=>{
-            expect(error.currency).toBeDefined(); 
-        });
-        it('Approximate value is required',()=>{
-            expect(error.approxValue).toBeDefined(); 
-        });
-        it('Contract type is required',()=>{
-            expect(error.contractType).toBeDefined(); 
-        });
-        it('CDM notifiable is required',()=>{
-            expect(error.cdmNotify).toBeDefined(); 
-        });
-        it('Asset is required (First)',()=>{
-            expect(error.assetworkedonprimary).toBeDefined(); 
-        });
-        it('Asset is required (Second)',()=>{
-            expect(error.assetworkedonsecond).toBeDefined(); 
-        });
-        it('Asset is required (Third)',()=>{
-            expect(error.assetworkedonthird).toBeDefined(); 
-        });
-        */
   });
 });
