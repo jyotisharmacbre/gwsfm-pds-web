110a9dbc1e97eefe55a99456be9a0034fa49c50e
jyotisharmacbre <56389968+amitguptacbre@users.noreply.github.com>
2019-11-29 11:21:56 +0530

    added multiligual key

--- web/src/components/Tile/Quotes.tsx
+++ web/src/components/Tile/Quotes.tsx
@@ -10,18 +10,18 @@ class Quotes extends Component {
               <div className="col-lg-4 pl-md-2">
                 <div className="card_wrap">
                   <div className="card">
-                    <h6 className="title">Quote 01</h6>
+                    <h6 className="title">TITLE_QUOTE1</h6>
                     <form>
                       <div className="form-group">
-                        <label>Supplier's Name</label>
+                        <label>LABEL_SUPPLIER1</label>
                         <input
                           className="form-control"
                           type="text"
-                          placeholder="Enter Name"
+                          placeholder="PLACEHOLDER_QUOTE_SUPPLIER1"
                         />
                       </div>
                       <div className="form-group">
-                        <label>Quote Value (&#163;)</label>
+                        <label>LABEL_QUOTE1</label>
                         <input
                           className="form-control"
                           type="text"
@@ -35,18 +35,18 @@ class Quotes extends Component {
               <div className="col-lg-4 pl-md-2">
                 <div className="card_wrap">
                   <div className="card">
-                    <h6 className="title">Quote 02</h6>
+                    <h6 className="title">TITLE_QUOTE2</h6>
                     <form>
                       <div className="form-group">
-                        <label>Supplier's Name</label>
+                        <label>LABEL_SUPPLIER2</label>
                         <input
                           className="form-control"
                           type="text"
-                          placeholder="Enter Name"
+                          placeholder="PLACEHOLDER_QUOTE_SUPPLIER2"
                         />
                       </div>
                       <div className="form-group">
-                        <label>Quote Value (&#163;)</label>
+                        <label>LABEL_QUOTE2</label>
                         <input
                           className="form-control"
                           type="text"
@@ -60,18 +60,18 @@ class Quotes extends Component {
               <div className="col-lg-4 pl-md-2 pr-lg-2">
                 <div className="card_wrap">
                   <div className="card">
-                    <h6 className="title">Quote 03</h6>
+                    <h6 className="title">TITLE_QUOTE3</h6>
                     <form>
                       <div className="form-group">
-                        <label>Supplier's Name</label>
+                        <label>LABEL_SUPPLIER3</label>
                         <input
                           className="form-control"
                           type="text"
-                          placeholder="Enter Name"
+                          placeholder=""
                         />
                       </div>
                       <div className="form-group">
-                        <label>Quote Value (&#163;)</label>
+                        <label>LABEL_QUOTE3</label>
                         <input
                           className="form-control"
                           type="text"
