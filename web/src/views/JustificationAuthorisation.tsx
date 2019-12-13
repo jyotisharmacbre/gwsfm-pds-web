e917f2400281f0d0074ab5dcf6d8035b34415b7d
Prakash Chandra Dhyani <PrakashChandra.Dhyani@cbre.com>
2019-12-13 13:30:29 +0530

    #Bug10644 - Not able to redirect from J&A

--- web/src/views/JustificationAuthorisation.tsx
+++ web/src/views/JustificationAuthorisation.tsx
@@ -1,5 +1,5 @@
 import { Grid } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { injectIntl, FormattedMessage } from 'react-intl';
 import CardContainer from '../components/CardContainer/CardContainer';
 import MultipleChart from '../components/Charts/MultipleCharts';
@@ -14,117 +14,53 @@ import { Link } from 'react-router-dom';
 import { Tile } from '../components/Tile';
 import { Row, Col } from 'react-bootstrap';
 import { formatMessage } from '../Translations/connectedIntlProvider';
+import { useHistory } from 'react-router-dom';
 
-class JustificationAuthorisation extends React.Component<IReactIntl> {
-  GetButtons() {
-    const action1: IBtnActionProps = {
-      Title: 'Create A New Project',
-      Icon: 'create',
-      Color: 'primary',
-      LinkTo: '/Project'
-    };
-    const action2: IBtnActionProps = {
-      Title: 'Pipeline',
-      Icon: 'pipeline',
-      Color: 'secondary',
-      LinkTo: '/Pipeline'
-    };
-    return [action1, action2];
-  }
-
-  viewAll: IBtnActionProps = {
-    Title: 'View All',
-    Icon: '',
-    Color: 'secondary',
-    LinkTo: '/Pipeline'
-  };
-
-  createTableColumns() {
-    return [
-      {
-        title: 'Name',
-        field: 'name',
-        customFilterAndSearch: (term: any, rowData: any) =>
-          (term = rowData.name.length)
-      },
-      { title: 'Updated By', field: 'updatedby' },
-      { title: 'Date', field: 'date', type: 'date' },
-      {
-        title: 'Status',
-        field: 'status',
-        lookup: { 1: 'Draft', 2: 'Submitted', 3: 'Completed' }
-      }
-    ];
-  }
-
-  getTableData() {
-    return [
-      {
-        name: 'Russel street road works',
-        updatedby: 'Stacy Salter',
-        date: new Date('07/21/1987'),
-        status: 1
-      },
-      {
-        name: 'London Bridge aqueduct planning',
-        updatedby: 'Hame Moore',
-        date: new Date('07/21/1987'),
-        status: 1
-      },
-      {
-        name: '51 Southwark street refilling',
-        updatedby: 'Lucy Benner',
-        date: new Date('07/21/1987'),
-        status: 2
-      },
-      {
-        name: 'Church street reviewing',
-        updatedby: 'Imran Khan',
-        date: new Date('07/21/1987'),
-        status: 3
-      },
-      {
-        name: 'Russel street road works',
-        updatedby: 'Jacy Lue',
-        date: new Date('07/21/1987'),
-        status: 1
-      },
-      {
-        name: 'Gregory Nash approval',
-        updatedby: 'Stirlin Archer',
-        date: new Date('07/21/1987'),
-        status: 3
-      }
-    ];
-  }
-  render() {
-    return (
-      <div className="container-fluid">
-        <Row>
-          <Col lg={12}>
-            <div className="custom-wrap">
-              <div className="heading-subtitle">
-                <h1>
-                  <span className="d-md-block d-none">
-                    Justification &amp; Authorisation
-                  </span>{' '}
-                  <span className="d-md-none">J&amp;A</span>
-                </h1>
-                <p>
-                  Please select the section that is relevanto to your project.
-                </p>
-              </div>
-              <Tile></Tile>
-              <div className="mr-35 three-btn mt-4">
-                <button className="active" type="button"> <FormattedMessage id="BUTTON_PREVIOUS"></FormattedMessage></button>
-                <button type="button" name="next" className="ml-auto"><FormattedMessage id="BUTTON_SAVE"></FormattedMessage></button>
-              <button type="button" name="next" className=""><FormattedMessage id="BUTTON_NEXT"></FormattedMessage></button></div>
-            </div>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+interface IProps {
+  match: any;
 }
+const JustificationAuthorisation: React.FC<IProps> = (props: IProps) => {
+  let history = useHistory();
+  const redirectToPrelims = () => {
+    return history.push(`/preliminaries/${props.match.params.projectId}`);
+  };
+  return (
+    <div className="container-fluid">
+      <Row>
+        <Col lg={12}>
+          <div className="custom-wrap">
+            <div className="heading-subtitle">
+              <h1>
+                <span className="d-md-block d-none">
+                  Justification &amp; Authorisation
+                </span>{' '}
+                <span className="d-md-none">J&amp;A</span>
+              </h1>
+              <p>Please select the section that is relevant to your project.</p>
+            </div>
+            <Tile></Tile>
+            <div className="mr-35 three-btn mt-4">
+              <button className="active" type="button">
+                {' '}
+                <FormattedMessage id="BUTTON_PREVIOUS"></FormattedMessage>
+              </button>
+              <button type="button" name="next" className="ml-auto">
+                <FormattedMessage id="BUTTON_SAVE"></FormattedMessage>
+              </button>
+              <button
+                type="button"
+                name="next"
+                className=""
+                onClick={redirectToPrelims}
+              >
+                <FormattedMessage id="BUTTON_NEXT"></FormattedMessage>
+              </button>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-export default injectIntl(JustificationAuthorisation);
+export default JustificationAuthorisation;
