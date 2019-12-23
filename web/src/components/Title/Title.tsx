3ed551d41364e5f1f4482e3b86f84467ec54f765
jyotisharmacbre <56389968+amitguptacbre@users.noreply.github.com>
2019-12-23 18:01:13 +0530

    10477 : create review and approve page with binding data in project overview page

--- web/src/components/Title/Title.tsx
+++ web/src/components/Title/Title.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
 // import { FormattedMessage } from 'react-intl';
 
 export function MainTitle(props: any) {
   return (
-    <div className="col-md-6">
-      <h1>{props.children}</h1>
-    </div>
+    <div className="row align-items-center mb-3">
+                <div className="col-lg-6">
+                  <h1 className="mb-2">{props.children}</h1>
+                </div>
+                <div className="col-lg-6 d-flex justify-content-xl-end justify-content-start">
+                  <button
+                    className="download_pdf_btn"
+                    type="button"
+                  ><FontAwesomeIcon className="" icon={faDownload} /> DOWNLOAD PDF
+                  </button>
+                </div>
+              </div>
+    
   );
 }
 
