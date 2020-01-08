9343ecd879b9df233f67fcb688d5fcd014e1b970
Rishav Mittal <Rishav.Mittal@cbre.com>
2020-01-08 12:44:10 +0530

    10800:code changes to fix type issue

--- web/src/store/ProjectOverviewForm/DataWrapper.tsx
+++ web/src/store/ProjectOverviewForm/DataWrapper.tsx
@@ -21,7 +21,7 @@ export const setupInitialApprovalData = (payload) => {
         let approveRangeMapping = ProjectApproverTypeAndRangeMapping.find(x => x.type == lookupApproverType.lookupKey.toString());
         let approverRange =
             lookupdata.filter(x => x.lookupItem.toLowerCase() == LookupType.Project_Approval_Range.toLowerCase()
-                && x.lookupKey.toString() == approveRangeMapping?.range);
+                && x.lookupKey.toString() == (approveRangeMapping?approveRangeMapping.range:approveRangeMapping));
 
         let approval: IProjectApprovals = {
             projectId: projectId,
