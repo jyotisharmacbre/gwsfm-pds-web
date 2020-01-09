404e0ba4c86e95b84da460b7cc92f96c97e91602
Prakash Chandra Dhyani <PrakashChandra.Dhyani@cbre.com>
2020-01-09 10:21:27 +0530

    conflicts resolved and changes pushed

--- web/src/components/Forms/Subcontractor/SubContractorActivityForm.tsx
+++ web/src/components/Forms/Subcontractor/SubContractorActivityForm.tsx
@@ -31,125 +31,125 @@ import NewTypeAhead from '../../TypeAhead/NewTypeAhead';
 
 import ValidatedNumericInput from '../../../components/NumericInput/index';
 interface Props {
-  fields:any,
-  activities:Array<ISubContractorActivity>;
-  currencySymbol:string;
-  intl:any;
+  fields: any,
+  activities: Array<ISubContractorActivity>;
+  currencySymbol: string;
+  intl: any;
   getDynamicSubContractorData: (value: any, success: any, failure: any) => void;
 }
- 
-const SubContractorActivityForm :React.FC<Props> = (props:Props) => {
-  const {fields,intl} = props;
-  
-  const formatSubcontractForTypeAhead = (data) => {
-		let result: any = [];
-		data.map((subcontractData: any) => {
-			result.push({ label: subcontractData.name, id: subcontractData.subContractorId });
-		});
-		return result;
-  };
-  
-  return(
-    <div>
-    {fields.map((member, index) => (
-      <div className="row" key={index} data-test="sub-contractor-form">
-        <div className="col-lg-12">
-          <div className="forms_wrap">
-            {fields.length > 1 ? (
-              <button
-                data-test="deleteactivity"
-                className="delete_text"
-                onClick={
-                 ()=>confirmAlert({
-                  intl:props.intl,
-                  titleKey:"TITLE_CONFIRMATION",
-                  contentKey:"MESSAGE_SUBCONTRACTOR_DELETE_ACTIVITY",
-                  handleConfirm:()=>fields.remove(index)
-                })}
-              >
-                <FormattedMessage id='BUTTON_DELETE' />
-                <FontAwesomeIcon className="" icon={faTrash} />
-              </button>
-            ) : null}
-            <div className="row">
-              <div className="col-lg-7">
-                <Field
-                  name={`${member}.activityName`}
-                  data-test="activityName"
-                  type="text"
-                  component={PdsFormInput}
-                  validate={[
-                    Validate.maxLength(1000)
-                  ]}
-                  labelKey="LABEL_ACTIVITY_NAME"
-                  placeholderKey="PLACEHOLDER_ACTIVITY_NAME"
-                />
-                <Field
-                  name={`${member}.isExistingSubcontractor`}
-                  component={PdsFormButton}
-                  buttons={selectionButtons}
-                  labelKey="LABEL_EXISTING_SUBCONTRACTOR"
-                />
-                <NewTypeAhead
-									name={`${member}.subcontractorId`}
-									onSearch={props.getDynamicSubContractorData}
-									formatData={formatSubcontractForTypeAhead}
-									DynamicsType="subcontractorId"
-									placeholderKey="PLACEHOLDER_SUBCONTRACTOR"
-									labelName="LABEL_SUBCONTRACTOR"
-									submitParam="id"
-								/>
-                
-								{props.activities[0].subcontractorId === '0' && (
-									<Field
-										name={`${member}.otherSubcontractorName`}
-										type="text"
-										component={PdsFormInput}
-										labelKey="LABEL_OTHER_SUBCONTRACTOR"
-										placeholderKey="PLACEHOLDER_OTHER_SUBCONTRACTOR"
-									/>
-								)}
 
-                <Field
-                  name={`${member}.isPreferredSupplier`}
-                  component={PdsFormButton}
-                  buttons={selectionButtons}
-                  labelKey="LABEL_PREFERRED_SUPPLIER"
-                />
-                <Field
-                  name={`${member}.totalCost`}
-                  type="number"
-                  component={PdsFormInput}
-                  className="width-120 pl-20"
-                  validate={[
-                    Validate.maxLength(1000),
-                    onlyNumber
-                  ]}
-                  currency={props.currencySymbol}
-                  divPosition="relative"
-                  labelKey="LABEL_TOTAL_COST"
-                  placeholderKey=""
-                />
-                <Field
-                  name={`${member}.grossMargin`}
-                  type="number"
-                  component={PdsFormInput}
-                  className="width-120 pl-20"
-                  validate={[
-                    Validate.maxLength(1000),
-                    onlyNumber
-                  ]}
-                  currency="%"
-                  divPosition="relative"
-                  labelKey="LABEL_GROSS_MARGIN"
-                  placeholderKey=""
-                />
-                <Field
-                  name={`${member}.totalSell`}
-                  type="number"
-                  input={{
-                    value:calculateSell(props.activities[index].totalCost,props.activities[index].grossMargin),
-                    disabled:true
+const SubContractorActivityForm: React.FC<Props> = (props: Props) => {
+  const { fields, intl } = props;
+
+  const formatSubcontractForTypeAhead = (data) => {
+    let result: any = [];
+    data.map((subcontractData: any) => {
+      result.push({ label: subcontractData.name, id: subcontractData.subContractorId });
+    });
+    return result;
+  };
+
+  return (
+    <div>
+      {fields.map((member, index) => (
+        <div className="row" key={index} data-test="sub-contractor-form">
+          <div className="col-lg-12">
+            <div className="forms_wrap">
+              {fields.length > 1 ? (
+                <button
+                  data-test="deleteactivity"
+                  className="delete_text"
+                  onClick={
+                    () => confirmAlert({
+                      intl: props.intl,
+                      titleKey: "TITLE_CONFIRMATION",
+                      contentKey: "MESSAGE_SUBCONTRACTOR_DELETE_ACTIVITY",
+                      handleConfirm: () => fields.remove(index)
+                    })}
+                >
+                  <FormattedMessage id='BUTTON_DELETE' />
+                  <FontAwesomeIcon className="" icon={faTrash} />
+                </button>
+              ) : null}
+              <div className="row">
+                <div className="col-lg-7">
+                  <Field
+                    name={`${member}.activityName`}
+                    data-test="activityName"
+                    type="text"
+                    component={PdsFormInput}
+                    validate={[
+                      Validate.maxLength(1000)
+                    ]}
+                    labelKey="LABEL_ACTIVITY_NAME"
+                    placeholderKey="PLACEHOLDER_ACTIVITY_NAME"
+                  />
+                  <Field
+                    name={`${member}.isExistingSubcontractor`}
+                    component={PdsFormButton}
+                    buttons={selectionButtons}
+                    labelKey="LABEL_EXISTING_SUBCONTRACTOR"
+                  />
+                  <NewTypeAhead
+                    name={`${member}.subcontractorId`}
+                    onSearch={props.getDynamicSubContractorData}
+                    formatData={formatSubcontractForTypeAhead}
+                    DynamicsType="subcontractorId"
+                    placeholderKey="PLACEHOLDER_SUBCONTRACTOR"
+                    labelName="LABEL_SUBCONTRACTOR"
+                    submitParam="id"
+                  />
+
+                  {props.activities[0].subcontractorId === '0' && (
+                    <Field
+                      name={`${member}.otherSubcontractorName`}
+                      type="text"
+                      component={PdsFormInput}
+                      labelKey="LABEL_OTHER_SUBCONTRACTOR"
+                      placeholderKey="PLACEHOLDER_OTHER_SUBCONTRACTOR"
+                    />
+                  )}
+
+                  <Field
+                    name={`${member}.isPreferredSupplier`}
+                    component={PdsFormButton}
+                    buttons={selectionButtons}
+                    labelKey="LABEL_PREFERRED_SUPPLIER"
+                  />
+                  <ValidatedNumericInput
+                    name={`${member}.totalCost`}
+                    type="number"
+                    component={PdsFormInput}
+                    className="width-120 pl-20"
+                    validate={[
+                      Validate.maxLength(1000),
+                      onlyNumber
+                    ]}
+                    currency={props.currencySymbol}
+                    divPosition="relative"
+                    labelKey="LABEL_TOTAL_COST"
+                    placeholderKey=""
+                  />
+                  <ValidatedNumericInput
+                    name={`${member}.grossMargin`}
+                    type="number"
+                    component={PdsFormInput}
+                    className="width-120 pl-20"
+                    validate={[
+                      Validate.maxLength(1000),
+                      onlyNumber
+                    ]}
+                    currency="%"
+                    divPosition="relative"
+                    labelKey="LABEL_GROSS_MARGIN"
+                    placeholderKey=""
+                  />
+                  <Field
+                    name={`${member}.totalSell`}
+                    type="number"
+                    input={{
+                      value: calculateSell(props.activities[index].totalCost, props.activities[index].grossMargin),
+                      disabled: true
                     }}
                     component={PdsFormInput}
                     className="width-120 pl-20"
