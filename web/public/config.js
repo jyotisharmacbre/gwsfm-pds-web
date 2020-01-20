8e10e72da99d86d57225c8c2b019fab42cca3625
Arun Saini <arun.saini1@cbre.com>
2020-01-20 13:32:12 +0530

    Fix test cases

--- web/public/config.js
+++ web/public/config.js
@@ -4,6 +4,6 @@ window.Config = {
     REACT_APP_AUTH_SIGNINPOLICY: "#{AUTH_SIGNINPOLICY}#",
     REACT_APP_AUTH_APPID: "#{AUTH_APPID}#",
     REACT_APP_INSIGHTS_KEY:"#{APP_INSIGHTS_KEY}#",
-    REACT_APP_API_BASE_URL: 'https://qat-pds-middletier.azurewebsites.net',
-    REACT_APP_USER_SERVICE_URL: 'https://qat-foundation-gateway.azurewebsites.net'  
+    REACT_APP_API_BASE_URL: '#{REACT_APP_API_BASE_URL}#',
+    REACT_APP_USER_SERVICE_URL: '#{REACT_APP_USER_SERVICE_URL}#' 
   };
