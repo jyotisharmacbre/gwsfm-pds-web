bca2ce837a2dced1db7efaea07b26c155d0f68c7
Rishav Mittal <Rishav.Mittal@cbre.com>
2020-01-27 13:31:02 +0530

    10616: code changes to add test cases and refactor

---
+++ web/src/components/DatePicker/Test/ReactDates.test.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ReactDates, { validDate } from '../ReactDates';
+
+describe('ReactDates test cases', () => {
+  let wrapper: any;
+  beforeEach(() => {
+    const props:any= {
+        input:  {value:{
+            date(){
+return 24;
+        },
+        month(){
+            return 1;
+        },
+        year(){
+            return 2020
+        },
+        onChange:jest.fn()
+    }},
+        meta: { touched:true, error:false, warning:false },
+        placeholder: '',
+        disabled: '',
+        required: ''
+    };
+    wrapper = shallow(<ReactDates {...props} />);
+  });
+  it('Defines the component', () => {
+    expect(wrapper).toBeDefined();
+  });
+  it('should return false if selected date is less than today date', () => {
+    let expectedResult=false;
+    let result=validDate(2020,1,25);
+    expect(result).toBe(expectedResult);
+});
+it('should return false if selected date is greater than today date', () => {
+  let expectedResult=true;
+  let result=validDate(2020,1,28);
+  expect(result).toBe(expectedResult);
+});
+it('should return false if year is not a number', () => {
+  let expectedResult=false;
+  let result=validDate(NaN,1,28);
+  expect(result).toBe(expectedResult);
+});
+it('should return false if month is not a number', () => {
+  let expectedResult=false;
+  let result=validDate(2020,NaN,28);
+  expect(result).toBe(expectedResult);
+});
+it('should return false if day is not a number', () => {
+  let expectedResult=false;
+  let result=validDate(2020,1,NaN);
+  expect(result).toBe(expectedResult);
+});
+});
