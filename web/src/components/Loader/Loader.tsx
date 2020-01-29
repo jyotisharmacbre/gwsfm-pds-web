e6f72cdd9915ac3ce5aba0775da7638e7cf3b322
jyotisharmacbre <56389968+amitguptacbre@users.noreply.github.com>
2020-01-29 10:33:18 +0530

    12171 : Loader image creation

---
+++ web/src/components/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+class Loader extends React.Component {
+    render() {
+      return (
+        <div className="sbl-circ-path"></div>
+      );
+    }
+  }
+
+  export default Loader;
