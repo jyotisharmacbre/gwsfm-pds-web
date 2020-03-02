4b3b95e4f9d317cc9ababc186f851d98f914e863
Neha Meena <Neha.Meena@cbre.com>
2020-03-02 16:02:14 +0530

    Test set Up cloned

---
+++ web/src/components/Forms/ProjectOverviewForm/Test/ProjectApprovalFormTestData.tsx
@@ -0,0 +1,19 @@
+import { IProjectOverviewState } from '../../../../store/ProjectOverviewForm/Types/IProjectOverviewState';
+import Notify from '../../../../enums/Notify';
+import EventType from '../../../../enums/EventType';
+import { IProjectApprovals } from '../../../../store/ProjectOverviewForm/Types/IProjectApprovals';
+
+export const projectApprovalsList: Array<IProjectApprovals> =  [
+    {
+      projectApprovalId: '1',
+      projectId : '1',
+       projectApprovalRange : 1,
+       projectApprovalRangeDescription : '1',
+       approverType: 1,
+       approverTypeDescription: 'string',
+       approvalStatus: 4,
+       approvalStatusDescription: 'string',
+       userId: 'string',
+       showRangeLabel: true,
+    }
+  ];
