260d22395aa0e7e5cb369582f46a23cdf1fff65c
avneetsinghcbre <57431154+avneetsinghcbre@users.noreply.github.com>
2020-03-05 17:45:45 +0530

    Added Toaster message time out in config

--- web/public/config.js
+++ web/public/config.js
@@ -1,8 +1,9 @@
 window.Config = {
-    REACT_APP_AUTHORITY:"https://login.microsoftonline.com/#{AzureADTenantId}#",
-    REACT_APP_CLIENT_ID:"#{PDSClientId}#",
-    REACT_APP_INSIGHTS_KEY:"#{PDS-AppInsight-Key}#",
+    REACT_APP_AUTHORITY: "https://login.microsoftonline.com/#{AzureADTenantId}#",
+    REACT_APP_CLIENT_ID: "#{PDSClientId}#",
+    REACT_APP_INSIGHTS_KEY: "#{PDS-AppInsight-Key}#",
     REACT_APP_MIDDLETIER_URL: '#{REACT_APP_MIDDLETIER_URL}#',
     REACT_APP_GATEWAY_URL: '#{REACT_APP_GATEWAY_URL}#',
-    REACT_APP_DATE_FORMAT:'MM/DD/YYYY'
-  };
+    REACT_APP_DATE_FORMAT: 'MM/DD/YYYY',
+    TOASTER_TIME_OUT:'5000'
+};
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import Layout from './components/Layouts/Layout';
-import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
-import { ToastContainer, Zoom, Bounce } from 'react-toastify';
+import { ToastContainer, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { injectIntl } from 'react-intl';
 import ErrorBoundary from './components/Error/ErrorBoundary';
@@ -10,15 +9,17 @@ import { loadProgressBar } from 'axios-progress-bar'
 import 'axios-progress-bar/dist/nprogress.css'
 import { baseAPI } from './client';
 import preval from 'preval.macro';
+import appConfig from './helpers/config-helper';
 
 const App: React.FC = (props: any) => {
+	const config = appConfig();
 	//Loading progressbar
 	loadProgressBar({ showSpinner: false }, baseAPI);
 	return (
 		<ErrorBoundary showPage={true}> 
 			<div>
 				<Layout />
-				<ToastContainer autoClose={5000} className="custom_toast_block" transition={Bounce} />
+				<ToastContainer autoClose={parseInt(config.TOASTER_TIME_OUT)} className="custom_toast_block" transition={Bounce} />
 				<p style={{ display: 'none' }}>
 					Build Date: {preval`module.exports = new Date().toLocaleString();`}.
         		</p>
