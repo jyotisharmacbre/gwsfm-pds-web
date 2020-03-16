249c9044aaea1cea90a1e1997a70d831159202e7
avneetsinghcbre <57431154+avneetsinghcbre@users.noreply.github.com>
2020-03-16 16:33:53 +0530

    config variable removed

--- web/src/contexts/AuthProvider/AuthService.js
+++ web/src/contexts/AuthProvider/AuthService.js
@@ -1,7 +1,7 @@
 import { MsalAuthProvider, LoginType } from 'react-aad-msal';
 import appConfig from '../../helpers/config-helper';
 
-const aadConfig = appConfig(); 
+const aadConfig = appConfig();
 // Msal Configurations
 const config = {
     auth: {
@@ -14,20 +14,19 @@ const config = {
         storeAuthStateInCookie: false
     }
 };
- 
+
 // Authentication Parameters
 const authenticationParameters = {}
 
- 
+
 // Options
 const options = {
     loginType: LoginType.Redirect,
     tokenRefreshUri: window.location.origin
 }
 function getMsalAuthProvider() {
-    if (aadConfig.REACT_APP_TEST == undefined || aadConfig.REACT_APP_TEST == 'false')
-        return new MsalAuthProvider(config, authenticationParameters, options);
-    else
+    if (aadConfig.REACT_APP_TEST != undefined)
         return {};
+    return new MsalAuthProvider(config, authenticationParameters, options);
 }
 export const msalAuthProvider = getMsalAuthProvider();
